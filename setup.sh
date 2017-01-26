da7664338930d40ccd979fc7adc1078dfa9008e9
Anatolii Saevskii <a.saevskiy@cian.ru>
2017-01-26 16:57:24 +0300

    run mysql on all hosts

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@ sudo apt-get -y  install git  m4 make dh-autoreconf pkg-config mysql-server libm
 mkdir /opt/boinc && git clone https://github.com/BOINC/boinc.git /opt/boinc
 cd /opt/boinc && ./_autosetup && ./configure --disable-client --disable-manager && make
 cd /opt/boinc/tools && touch k.k && exec 6>&1 && exec > k.k && echo y && exec 1>&6 6>&-
-touch m.db.sc && exec 6>&1 && exec > m.db.sc && echo "GRANT ALL ON *.* TO 'vagrant'@'localhost';" && echo "SET PASSWORD FOR 'vagrant'@'localhost'='';" && exec 1>&6 6>&-
-mysql -h localhost -u root --password=pass666666 < m.db.sc
+touch m.db.sc && exec 6>&1 && exec > m.db.sc && echo "GRANT ALL ON *.* TO 'vagrant'@'%';" && echo "SET PASSWORD FOR 'vagrant'@'%'='';" && exec 1>&6 6>&-
+mysql -h localhost -u root --password=pass666 < m.db.sc
 ./make_project  --url_base=http://192.168.33.10/ --db_host=localhost --db_user=vagrant --project_root=/bp_src/mbp  mbp < k.k
 cd /opt/boinc/tools && rm k.k && rm m.db.sc
 
